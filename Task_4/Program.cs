40f09eecfbe253bd4c26ab6208586ba38ff8da39
ValeryGG <v4lsuk@yandex.ru>
2022-02-10 11:59:38 +0300

    Second_task_done!

---
+++ Task_4/Program.cs
@@ -0,0 +1,13 @@
+﻿int a, b, c;
+
+Console.WriteLine("Введите числа ");
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+
+if (b > max) max = b;
+
+if (c > max) max = c;
+
+Console.WriteLine("Максимальное из трёх чисел - " + max);
