2223d5b645b782a3322ba43c8457700d87ca0a29
ValeryGG <v4lsuk@yandex.ru>
2022-02-10 12:19:47 +0300

    Task12_done!

---
+++ Task_12/Program.cs
@@ -0,0 +1,10 @@
+﻿// Удалить вторую цифру трёхзначного числа
+int a;
+
+Console.WriteLine("Введите число ");
+a = Convert.ToInt32(Console.ReadLine());
+
+int half = a % 10; 
+int half1 = a / 100;
+
+Console.WriteLine("Число без второй цифры - " + half1 + half);
