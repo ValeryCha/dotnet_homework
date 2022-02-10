b9ef7fd6dcb1a69ebdb53f728050c7c9ddb93983
ValeryGG <v4lsuk@yandex.ru>
2022-02-10 11:58:41 +0300

    First_task_done!

---
+++ Task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿int max, min;
+
+Console.WriteLine("Введите два числа ");
+max = Convert.ToInt32(Console.ReadLine());
+min = Convert.ToInt32(Console.ReadLine());
+
+if (max > min)
+{
+    Console.WriteLine("Наибольшее число " + max);
+    Console.WriteLine("Наименьшее число " + min);
+}
+else if (max < min)
+{
+    Console.WriteLine("Наименьшее число " + max);
+    Console.WriteLine("Наибольшее число " + min);
+}
+else if (max == min)
+{
+     Console.WriteLine("Числа равны ");
+}
