215aeb4263d85f9154073aad42658542c117f53f
ValeryGG <v4lsuk@yandex.ru>
2022-02-10 12:38:32 +0300

    Task14_done!

---
+++ Task_14/Program.cs
@@ -0,0 +1,12 @@
+﻿//  Найти третью цифру числа или сообщить, что её нет.
+
+Random rnd = new Random();
+int a = rnd.Next(1, 999);
+Console.WriteLine(a);
+string b = Convert.ToString(a);
+if (b.Length > 2)
+{
+    Console.WriteLine(b[2]);
+}
+else 
+Console.WriteLine("Третья цифра отсутсвует ");
