fd78152e202945a1c564616eec7633d720e7bd44
ValeryGG <v4lsuk@yandex.ru>
2022-02-10 12:12:30 +0300

    Task10_done!

---
+++ Task_10/Program.cs
@@ -0,0 +1,11 @@
+﻿int a;
+
+Console.WriteLine("Введите число ");
+a = Convert.ToInt32(Console.ReadLine());
+
+int half = a / 10; 
+int half1 = half % 10;
+
+Console.WriteLine("Вторая цифра трехзнакочного числа - " + half1);
+
+    
